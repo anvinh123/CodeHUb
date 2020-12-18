cfad3e8ed1c2db675be3f11e8f1338cd30a221d7
anvinh123 <75969515+anvinh123@users.noreply.github.com>
2020-12-18 16:37:28 +0700

    Update README.py

--- README.py
+++ README.py
@@ -9,7 +9,6 @@ print(data.head(5))
 print('\n')
 print(data.dtypes)
 
-
 # ------------------------------------------------
 # OVERVIEWING PRODUCT IN DB
 
@@ -19,7 +18,6 @@ product_amount=data.groupby(by=['product_name'],as_index=False).agg({
     'customer_id': 'count'
 })
 product_amount.sort_values(by=['amount'],ascending=False)[:10]
-
 
 # ------------------------------------------------ CONVERTING DATETIME TYPE HAVING UTC --------- VER 1
 # HARD REMOVING UTC IN DATETIME 
@@ -60,17 +58,38 @@ db_amount['avg_item']= round(db_amount['units']/db_amount['order_id'],2)
 db_amount['amount']=db_amount['amount'].map('{:,.2f}'.format)
 db_amount[:10]
 
-
 # ------------------------------------- MULTIPLES FILTERS -----------------------------------
 fill=['1','440','631','555']
 db_amount[['date_created','customer_id','amount','units']][db_amount.customer_id.isin(fill)]
 
+d_time_group['date']=pd.to_datetime(d_time_group['date'])
+d_time_group['year']=pd.DatetimeIndex(d_time_group['date']).year
+d_time_group['month']=pd.DatetimeIndex(d_time_group['date']).month
+d_time_group['day']=pd.DatetimeIndex(d_time_group['date']).day
+d_time_group['new_g']= d_time_group['year']++d_time_group['month']
+d_time_group[d_time_group.day >=13][d_time_group.month == 12]
 
+# ------------------------------------- CONCAT MULTIPLES COLUMNS ----------------------
+d_time_group['year_month']=d_time_group[['year','month']].apply(lambda x : '{}{}'.format(x[0],x[1]),axis=1)
 
+# --------------------------------------- BAR CHART ----------------------------------
+from matplotlib import pyplot as plt
+from matplotlib import style
 
+style.use('ggplot')
+x=d_time_group['day']
+y=d_time_group['amount']
+fig, ax = plt.subplots()
 
+ax.bar(x,y, align = 'center')
+ax.set_title ('Revvenue Daily')
+ax.set_xtitle('Date')
+ax.set_ytitle('Amount')
 
-
+ax.set_xticks(x)
+ax.set_xticklabels(("amount"))
+plt.show()
+print('OK')
 
 
 
