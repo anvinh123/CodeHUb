f2b4161c5d0fbbb5709d8e5a629d13563bd7f13b
anvinh123 <75969515+anvinh123@users.noreply.github.com>
2020-12-21 15:28:47 +0700

    Update segment_customer_normalize.py

--- segment_customer_normalize.py
+++ segment_customer_normalize.py
@@ -27,3 +27,82 @@ for n_cluster in [4,5,6,7,8]:
         kmeans.labels_)
     
     print('Silhouette Score for %i Clusters: %0.4f' % (n_cluster, silhouette_avg))
+	
+kmeans = KMeans(n_clusters=4).fit(normalized_df[['TotalSales', 'OrderCount', 'AvgOrderValue']])
+
+four_cluster_df = normalized_df[['TotalSales', 'OrderCount', 'AvgOrderValue']].copy(deep=True)
+four_cluster_df['Cluster'] = kmeans.labels_
+
+cluster1_metrics = kmeans.cluster_centers_[0]
+cluster2_metrics = kmeans.cluster_centers_[1]
+cluster3_metrics = kmeans.cluster_centers_[2]
+cluster4_metrics = kmeans.cluster_centers_[3]
+
+data = [cluster1_metrics, cluster2_metrics, cluster3_metrics, cluster4_metrics]
+cluster_center_df = pd.DataFrame(data)
+
+cluster_center_df.columns = four_cluster_df.columns[0:3]
+
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['OrderCount'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['TotalSales'],
+c='blue')
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['OrderCount'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['TotalSales'],
+c='red')
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['OrderCount'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['TotalSales'],
+c='orange')
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['OrderCount'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['TotalSales'],
+c='green')
+plt.title('TotalSales vs. OrderCount Clusters')
+plt.xlabel('Order Count')
+plt.ylabel('Total Sales')
+plt.grid()
+plt.show()
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['OrderCount'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['AvgOrderValue'],
+c='blue')
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['OrderCount'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['AvgOrderValue'],
+c='red')
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['OrderCount'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['AvgOrderValue'],
+c='orange')
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['OrderCount'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['AvgOrderValue'],
+c='green')
+plt.title('AvgOrderValue vs. OrderCount Clusters')
+plt.xlabel('Order Count')
+plt.ylabel('Avg Order Value')
+plt.grid()
+plt.show()
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['TotalSales'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 0]['AvgOrderValue'],
+c='blue')
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['TotalSales'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 1]['AvgOrderValue'],
+c='red')
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['TotalSales'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 2]['AvgOrderValue'],
+c='orange')
+plt.scatter(
+four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['TotalSales'], 
+four_cluster_df.loc[four_cluster_df['Cluster'] == 3]['AvgOrderValue'],
+c='green')
+plt.title('AvgOrderValue vs. TotalSales Clusters')
+plt.xlabel('Total Sales')
+plt.ylabel('Avg Order Value')
+plt.grid()
+plt.show()
