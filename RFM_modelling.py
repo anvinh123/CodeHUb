05f0599012d3baadaa128a486c27e766c5fcc181
anvinh123 <75969515+anvinh123@users.noreply.github.com>
2020-12-22 17:28:05 +0700

    Update RFM_modelling.py

--- RFM_modelling.py
+++ RFM_modelling.py
@@ -70,3 +70,103 @@ recently_df.columns=['customer_id','LastPurchaseDate']
 recently_df.head(10)
 
 #-------------------------------------------------------------------
+#CALCULATE RECENTLY
+recently_df['Recency']=recently_df['LastPurchaseDate'].apply(lambda x:(now - x).days)
+recently_df[:10]
+
+
+#-------------------------------------------------------------------
+#DROP LastPurchaseDate by 1
+recently_df.drop('LastPurchaseDate',axis=1,inplace=True)
+
+#-------------------------------------------------------------------
+#FREQUENCY 
+#DROP DUPLICATE CUSTOMER_ID
+retail_us_copy=retail_us
+retail_us_copy.drop_duplicates(subset=['order_code','customer_id'], keep='first', inplace=True)
+#CALCULATE FREQUENCY OF PURCHASES
+frequency_df=retail_us_copy.groupby(by=['customer_id'],as_index=False)['order_code'].count()
+frequency_df.columns=['customer_id','Frequency']
+frequency_df[:10]
+
+#-------------------------------------------------------------------
+#NEED TO REVIEW FIRST
+retail_us[:10]
+
+#-------------------------------------------------------------------
+#CREAT RFM TABLE
+#MERGE RECENCY DATAFRAME WITH FREQUENCY DATAFRAME
+tem_df = recently_df.merge(frequency_df,on='customer_id')
+tem_df[:10]
+
+#-------------------------------------------------------------------
+#MERGE WITH MONETARY DATAFRAME TO GET A TABLE WITH HAVING 3 COLUMNS
+rfm_df = tem_df.merge(monetary_df,on ='customer_id')
+#USE customer_id as index
+rfm_df.set_index('customer_id',inplace=True)
+
+#-------------------------------------------------------------------
+#RFM QUANTILES
+quantiles=rfm_df.quantile(q=[0.5,0.85,0.95])
+quantiles
+
+#-------------------------------------------------------------------
+#CREATE DICTIONARY
+quantiles.to_dict()
+
+#-------------------------------------------------------------------
+#CREATION OF RFM SEGMENTS
+#ARGUMENTS ( X = VALUES, P = RECENCY , MONETARY_VALUE , FREQUENCY, D = QUARTILES DICT)
+def RScore(x,p,d):
+    if x <= d[p][0.5]:
+        return 4
+    elif x <= d[p][0.85]:
+        return 3
+    elif x <= d[p][0.98]:
+        return 2
+    else:
+        return 1
+#ARGUMENS (X = VALUE, P = RECENCY , MONETARY_VALUE, FREQUENCY , K = QUARTILES DICT)
+def FScore(x,p,d):
+    if x <= d[p][0.5]:
+        return 1
+    elif x <= d[p][0.85]:
+        return 2
+    elif x <= d[p][0.98]:
+        return 3
+    else:
+        return 4
+print('Done')
+
+#-------------------------------------------------------------------
+#CREATE RFM SEGMENTATION TABLE
+rfm_segmentation = rfm_df
+rfm_segmentation['R_Quartile'] = rfm_segmentation['Recency'].apply(RScore,
+                                                                   args=('Recency',quantiles,))
+rfm_segmentation['F_Quartile'] = rfm_segmentation['Frequency'].apply(FScore,
+                                                                    args = ('Frequency',quantiles,))
+rfm_segmentation['M_Quartile'] = rfm_segmentation['Monetary'].apply(FScore,
+                                                                   args = ('Monetary',quantiles,))
+
+#-------------------------------------------------------------------
+#TEST
+rfm_segmentation[:10]
+
+#-------------------------------------------------------------------
+
+#MAKE SCORES
+rfm_segmentation['RFMScore'] = rfm_segmentation.R_Quartile.map(str)\
+                            + rfm_segmentation.F_Quartile.map(str)\
+                            + rfm_segmentation.M_Quartile.map(str)
+rfm_segmentation[:10]
+
+
+#-------------------------------------------------------------------
+
+
+
+
+
+
+
+#-------------------------------------------------------------------
